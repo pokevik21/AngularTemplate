1eeffd605852a9175b8e02f632b03d7c1428b977
pokevik21 <pokevik21@gmail.com>
2020-09-15 14:04:26 +0200

    Seccion19

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 // TODO: cambiar base_url
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://vic-adminpro-backend.herokuapp.com/api'
 };
